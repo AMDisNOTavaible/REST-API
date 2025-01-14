32c88a47b68bb687ae88e0f88f7afee818ac0537
AMDisNOTavailable <161005519+AMDisNOTavaible@users.noreply.github.com>
2025-01-14 10:50:27 +0300

    Create ProductCreateDto.cs

---
+++ ProductCreateDto.cs
@@ -0,0 +1,9 @@
+namespace TryApi.Models
+{
+    public class ProductCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
